103155011e65e351541d0cf9a5d78038dbe27760
cubeninggen <hhybg@naver.com>
2023-07-05 22:19:36 +0900

    로그인 기능 구현

--- src/main/java/com/example/blog/entity/User.java
+++ src/main/java/com/example/blog/entity/User.java
@@ -1,4 +1,29 @@
 package com.example.blog.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
 public class User {
+    // @Id Id 필드 생성 수정
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
--- src/main/java/com/example/blog/repository/UserRepository.java
+++ src/main/java/com/example/blog/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.blog.repository;
 
-public class UserRepository {
+import com.example.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
